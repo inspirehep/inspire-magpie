1ef08dc8de14d40daa12651d92772b91fe4ce131
Jan Stypka <janstypka@gmail.com>
2016-04-15 11:59:19 +0200

    config: include embedding size in the paths

--- inspire_magpie/config.py
+++ inspire_magpie/config.py
@@ -1,16 +1,20 @@
 import os
 
+from magpie.config import EMBEDDING_SIZE
+
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(ROOT_DIR, 'data')
 LOG_FOLDER = os.path.join(os.environ['HOME'], 'keras-results')
 
-KEYWORD_WORD2VEC = os.path.join(DATA_DIR, 'keywords', 'word2vec100.gensim')
-KEYWORD_SCALER = os.path.join(DATA_DIR, 'keywords', 'scaler100.pickle')
+_dim = str(EMBEDDING_SIZE)
 
-CATEGORY_WORD2VEC = os.path.join(DATA_DIR, 'categories', 'word2vec100.gensim')
-CATEGORY_SCALER = os.path.join(DATA_DIR, 'categories', 'scaler100.pickle')
+KEYWORD_WORD2VEC = os.path.join(DATA_DIR, 'keywords', 'word2vec' + _dim + '.gensim')
+KEYWORD_SCALER = os.path.join(DATA_DIR, 'keywords', 'scaler' + _dim + '.pickle')
 
-EXPERIMENT_WORD2VEC = os.path.join(DATA_DIR, 'experiments', 'word2vec100.gensim')
-EXPERIMENT_SCALER = os.path.join(DATA_DIR, 'experiments', 'scaler100.pickle')
+CATEGORY_WORD2VEC = os.path.join(DATA_DIR, 'categories', 'word2vec' + _dim + '.gensim')
+CATEGORY_SCALER = os.path.join(DATA_DIR, 'categories', 'scaler' + _dim + '.pickle')
 
-NO_OF_LABELS = 10000
+EXPERIMENT_WORD2VEC = os.path.join(DATA_DIR, 'experiments', 'word2vec' + _dim + '.gensim')
+EXPERIMENT_SCALER = os.path.join(DATA_DIR, 'experiments', 'scaler' + _dim + '.pickle')
+
+NO_OF_LABELS = 1000
