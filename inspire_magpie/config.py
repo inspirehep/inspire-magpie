122e6d39a0e5a35c52f091b0ba27f82a39109c7e
Jan Aage Lavik <jan.age.lavik@cern.ch>
2016-04-28 16:08:16 +0200

    config: data directory fixes
    Signed-off-by: Jan Aage Lavik <jan.age.lavik@cern.ch>

--- inspire_magpie/config.py
+++ inspire_magpie/config.py
@@ -1,8 +1,8 @@
 import os
+import sys
 
-ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DATA_DIR = os.path.join(ROOT_DIR, 'data')
-LOG_FOLDER = os.path.join(os.environ['HOME'], 'keras-results')
+DATA_DIR = os.path.join(sys.prefix, 'inspire_magpie_data')
+LOG_FOLDER = os.path.join(sys.prefix, 'var', 'log', 'keras-results')
 
 WORD2VEC_PATH = os.path.join(DATA_DIR, 'word2vec.gensim')
 SCALER_PATH = os.path.join(DATA_DIR, 'scaler.pickle')
--- setup.py
+++ setup.py
@@ -90,14 +90,24 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     # package_data={
-    #     'sample': ['package_data.dat'],
+    #     '': ['data/*/*'],
     # },
-
+    # include_package_data=True,
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
+    data_files=[('inspire_magpie_data', [
+        'data/scaler.pickle',
+        'data/word2vec.gensim',
+        'data/word2vec.gensim.syn0.npy',
+    ]), ('inspire_magpie_data/experiments', [
+        'data/experiments/model.pickle',
+    ]), ('inspire_magpie_data/keywords', [
+        'data/keywords/model.pickle',
+    ]), ('inspire_magpie_data/categories', [
+        'data/categories/model.pickle',
+    ])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
